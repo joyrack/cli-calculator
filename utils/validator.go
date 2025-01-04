844cd6e1d0db8afd688569710b0559fdcd10d5fb
Chirayu Sharma <chirayu.sharma@iongroup.com>
2025-01-04 21:56:13 +0530

    evaluate command checks if the expression contains valid characters

--- utils/validator.go
+++ utils/validator.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 
 	"github.com/spf13/cobra"
@@ -29,5 +30,29 @@ func ValidateOperands(command *cobra.Command, args []string) error {
 
 // To be used for the 'evaluate' command
 func ValidateExpression(command *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide 1 argument")
+	}
+
+	allowedChars := []rune{'(', ')', '+', '-', '*', '/', ' '}
+
+	expression := args[0]
+	for _, char := range expression {
+		if contains(allowedChars, char) {
+			continue
+		}
+		if _, err := strconv.Atoi(string(char)); err != nil {
+			return fmt.Errorf("%c is an invalid character", char)
+		}
+	}
 	return nil
+}
+
+func contains(charList []rune, char rune) bool {
+	for _, val := range charList {
+		if val == char {
+			return true
+		}
+	}
+	return false
 }
